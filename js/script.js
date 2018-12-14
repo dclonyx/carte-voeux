9eb99caab887768763931b1e783df0c206a1d40f
dclonyx <dcl.fabieno@18pixel.fr>
2018-12-14 16:58:11 +0100

    version finale

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var icone = document.getElementById('icone');
+var formulaire = document.getElementById('formulaire'); 
+
+icone.addEventListener("click",function(){
+    if (formulaire.style.display == "none"){
+        formulaire.style.display="block";
+    } else{
+        formulaire.style.display="none";
+    }
+}); 
